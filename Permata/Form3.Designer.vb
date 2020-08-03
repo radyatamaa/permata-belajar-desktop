176ec8ecc2e1d812be1df3f580b9f5105dceee80
bagusshndr <bagussuhendri19@gmail.com>
2020-08-03 19:42:24 +0700

    update ringkasan dan video

---
+++ Permata/Form3.Designer.vb
@@ -0,0 +1,192 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form3
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.dropdown4 = New System.Windows.Forms.Panel()
+        Me.Button11 = New System.Windows.Forms.Button()
+        Me.Button12 = New System.Windows.Forms.Button()
+        Me.Button13 = New System.Windows.Forms.Button()
+        Me.Button14 = New System.Windows.Forms.Button()
+        Me.Button15 = New System.Windows.Forms.Button()
+        Me.Button16 = New System.Windows.Forms.Button()
+        Me.Button17 = New System.Windows.Forms.Button()
+        Me.Button18 = New System.Windows.Forms.Button()
+        Me.Button19 = New System.Windows.Forms.Button()
+        Me.Button20 = New System.Windows.Forms.Button()
+        Me.dropdown4.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'dropdown4
+        '
+        Me.dropdown4.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.dropdown4.Controls.Add(Me.Button11)
+        Me.dropdown4.Controls.Add(Me.Button12)
+        Me.dropdown4.Controls.Add(Me.Button13)
+        Me.dropdown4.Controls.Add(Me.Button14)
+        Me.dropdown4.Controls.Add(Me.Button15)
+        Me.dropdown4.Controls.Add(Me.Button16)
+        Me.dropdown4.Controls.Add(Me.Button17)
+        Me.dropdown4.Controls.Add(Me.Button18)
+        Me.dropdown4.Controls.Add(Me.Button19)
+        Me.dropdown4.Controls.Add(Me.Button20)
+        Me.dropdown4.Location = New System.Drawing.Point(29, 44)
+        Me.dropdown4.Name = "dropdown4"
+        Me.dropdown4.Size = New System.Drawing.Size(767, 463)
+        Me.dropdown4.TabIndex = 38
+        Me.dropdown4.Visible = False
+        '
+        'Button11
+        '
+        Me.Button11.BackColor = System.Drawing.Color.White
+        Me.Button11.FlatAppearance.BorderSize = 0
+        Me.Button11.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button11.Location = New System.Drawing.Point(6, 360)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(753, 40)
+        Me.Button11.TabIndex = 32
+        Me.Button11.UseVisualStyleBackColor = False
+        '
+        'Button12
+        '
+        Me.Button12.BackColor = System.Drawing.Color.White
+        Me.Button12.FlatAppearance.BorderSize = 0
+        Me.Button12.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button12.Location = New System.Drawing.Point(6, 406)
+        Me.Button12.Name = "Button12"
+        Me.Button12.Size = New System.Drawing.Size(753, 40)
+        Me.Button12.TabIndex = 31
+        Me.Button12.UseVisualStyleBackColor = False
+        '
+        'Button13
+        '
+        Me.Button13.BackColor = System.Drawing.Color.White
+        Me.Button13.FlatAppearance.BorderSize = 0
+        Me.Button13.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button13.Location = New System.Drawing.Point(6, 2)
+        Me.Button13.Name = "Button13"
+        Me.Button13.Size = New System.Drawing.Size(753, 40)
+        Me.Button13.TabIndex = 30
+        Me.Button13.UseVisualStyleBackColor = False
+        '
+        'Button14
+        '
+        Me.Button14.BackColor = System.Drawing.Color.White
+        Me.Button14.FlatAppearance.BorderSize = 0
+        Me.Button14.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button14.Location = New System.Drawing.Point(6, 46)
+        Me.Button14.Name = "Button14"
+        Me.Button14.Size = New System.Drawing.Size(753, 40)
+        Me.Button14.TabIndex = 29
+        Me.Button14.UseVisualStyleBackColor = False
+        '
+        'Button15
+        '
+        Me.Button15.BackColor = System.Drawing.Color.White
+        Me.Button15.FlatAppearance.BorderSize = 0
+        Me.Button15.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button15.Location = New System.Drawing.Point(6, 90)
+        Me.Button15.Name = "Button15"
+        Me.Button15.Size = New System.Drawing.Size(753, 40)
+        Me.Button15.TabIndex = 28
+        Me.Button15.UseVisualStyleBackColor = False
+        '
+        'Button16
+        '
+        Me.Button16.BackColor = System.Drawing.Color.White
+        Me.Button16.FlatAppearance.BorderSize = 0
+        Me.Button16.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button16.Location = New System.Drawing.Point(6, 135)
+        Me.Button16.Name = "Button16"
+        Me.Button16.Size = New System.Drawing.Size(753, 40)
+        Me.Button16.TabIndex = 27
+        Me.Button16.UseVisualStyleBackColor = False
+        '
+        'Button17
+        '
+        Me.Button17.BackColor = System.Drawing.Color.White
+        Me.Button17.FlatAppearance.BorderSize = 0
+        Me.Button17.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button17.Location = New System.Drawing.Point(6, 180)
+        Me.Button17.Name = "Button17"
+        Me.Button17.Size = New System.Drawing.Size(753, 40)
+        Me.Button17.TabIndex = 26
+        Me.Button17.UseVisualStyleBackColor = False
+        '
+        'Button18
+        '
+        Me.Button18.BackColor = System.Drawing.Color.White
+        Me.Button18.FlatAppearance.BorderSize = 0
+        Me.Button18.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button18.Location = New System.Drawing.Point(6, 225)
+        Me.Button18.Name = "Button18"
+        Me.Button18.Size = New System.Drawing.Size(753, 40)
+        Me.Button18.TabIndex = 25
+        Me.Button18.UseVisualStyleBackColor = False
+        '
+        'Button19
+        '
+        Me.Button19.BackColor = System.Drawing.Color.White
+        Me.Button19.FlatAppearance.BorderSize = 0
+        Me.Button19.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button19.Location = New System.Drawing.Point(6, 270)
+        Me.Button19.Name = "Button19"
+        Me.Button19.Size = New System.Drawing.Size(753, 40)
+        Me.Button19.TabIndex = 24
+        Me.Button19.UseVisualStyleBackColor = False
+        '
+        'Button20
+        '
+        Me.Button20.BackColor = System.Drawing.Color.White
+        Me.Button20.FlatAppearance.BorderSize = 0
+        Me.Button20.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button20.Location = New System.Drawing.Point(6, 315)
+        Me.Button20.Name = "Button20"
+        Me.Button20.Size = New System.Drawing.Size(753, 40)
+        Me.Button20.TabIndex = 23
+        Me.Button20.UseVisualStyleBackColor = False
+        '
+        'Form3
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1617, 835)
+        Me.Controls.Add(Me.dropdown4)
+        Me.Name = "Form3"
+        Me.Text = "Form3"
+        Me.dropdown4.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents dropdown4 As Panel
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents Button13 As Button
+    Friend WithEvents Button14 As Button
+    Friend WithEvents Button15 As Button
+    Friend WithEvents Button16 As Button
+    Friend WithEvents Button17 As Button
+    Friend WithEvents Button18 As Button
+    Friend WithEvents Button19 As Button
+    Friend WithEvents Button20 As Button
+End Class
