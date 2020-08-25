0ae8142950145c6f4d48bbda43107a1f878a7e6e
bagusshndr <bagussuhendri19@gmail.com>
2020-08-25 23:54:38 +0700

    push isi ringkasan parah sih ini mah

--- Permata/IsiRIngkasan.Designer.vb
+++ Permata/IsiRIngkasan.Designer.vb
@@ -23,61 +23,47 @@ Partial Class IsiRIngkasan
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(IsiRIngkasan))
-        Me.PictureBox0 = New System.Windows.Forms.PictureBox()
         Me.Guna2Button7 = New Guna.UI2.WinForms.Guna2Button()
+        Me.PictureBox0 = New System.Windows.Forms.PictureBox()
         CType(Me.PictureBox0, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
-        '
-        'PictureBox0
-        '
-        Me.PictureBox0.Location = New System.Drawing.Point(2, 1)
-        Me.PictureBox0.Name = "PictureBox0"
-        Me.PictureBox0.Size = New System.Drawing.Size(197, 58)
-        Me.PictureBox0.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
-        Me.PictureBox0.TabIndex = 0
-        Me.PictureBox0.TabStop = False
         '
         'Guna2Button7
         '
         Me.Guna2Button7.AutoRoundedCorners = True
         Me.Guna2Button7.BackColor = System.Drawing.Color.Transparent
-        Me.Guna2Button7.BorderRadius = 25
+        Me.Guna2Button7.BorderRadius = 35
         Me.Guna2Button7.CheckedState.Parent = Me.Guna2Button7
         Me.Guna2Button7.CustomImages.Parent = Me.Guna2Button7
         Me.Guna2Button7.FillColor = System.Drawing.Color.Transparent
-        Me.Guna2Button7.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        resources.ApplyResources(Me.Guna2Button7, "Guna2Button7")
         Me.Guna2Button7.ForeColor = System.Drawing.Color.White
         Me.Guna2Button7.HoverState.Parent = Me.Guna2Button7
         Me.Guna2Button7.Image = CType(resources.GetObject("Guna2Button7.Image"), System.Drawing.Image)
         Me.Guna2Button7.ImageSize = New System.Drawing.Size(30, 30)
-        Me.Guna2Button7.Location = New System.Drawing.Point(2, 1)
-        Me.Guna2Button7.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Guna2Button7.Name = "Guna2Button7"
         Me.Guna2Button7.PressedColor = System.Drawing.Color.White
         Me.Guna2Button7.ShadowDecoration.Parent = Me.Guna2Button7
-        Me.Guna2Button7.Size = New System.Drawing.Size(53, 58)
-        Me.Guna2Button7.TabIndex = 55
         Me.Guna2Button7.UseTransparentBackground = True
+        '
+        'PictureBox0
+        '
+        resources.ApplyResources(Me.PictureBox0, "PictureBox0")
+        Me.PictureBox0.Name = "PictureBox0"
+        Me.PictureBox0.TabStop = False
         '
         'IsiRIngkasan
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.AutoScroll = True
-        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
-        Me.ClientSize = New System.Drawing.Size(884, 601)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None
+        resources.ApplyResources(Me, "$this")
         Me.Controls.Add(Me.Guna2Button7)
         Me.Controls.Add(Me.PictureBox0)
-        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Name = "IsiRIngkasan"
-        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
-        Me.Text = "IsiRIngkasan"
         CType(Me.PictureBox0, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-
-    Friend WithEvents PictureBox0 As PictureBox
     Friend WithEvents Guna2Button7 As Guna.UI2.WinForms.Guna2Button
+    Friend WithEvents PictureBox0 As PictureBox
 End Class
